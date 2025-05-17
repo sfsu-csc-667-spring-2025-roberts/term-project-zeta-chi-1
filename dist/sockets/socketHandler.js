d1f3619835f9c25c6c1515ea32e70b54aa4c4ccc
Alex3096 <bescobar5@sfsu.edu>
2025-05-17 12:09:45 -0700

    chat room implementation

--- dist/sockets/socketHandler.js
+++ dist/sockets/socketHandler.js
@@ -1,32 +1,37 @@
 "use strict";
 // src/sockets/socketHandler.ts
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.initializeSocketIO = initializeSocketIO;
 const socket_io_1 = require("socket.io");
 const matchmaking_1 = require("./matchmaking");
+const chat_1 = __importDefault(require("./chat"));
 function initializeSocketIO(httpServer, sessionMiddleware) {
     const io = new socket_io_1.Server(httpServer, {
         cors: {
-            origin: "*", // Configure this for production but for localhost it's fine
+            origin: "*",
             methods: ["GET", "POST"]
         }
     });
-    // Wrap express middleware so it's accessible to SocketIO
     const wrap = (middleware) => (socket, next) => middleware(socket.request, {}, next);
     io.use(wrap(sessionMiddleware));
-    // Auth middleware check for SocketIO connections
     io.use((socket, next) => {
         const session = socket.request.session;
-        if (session && session.user) { // Runs if user has session
+        if (session && session.user) {
             next();
         }
-        else { // User had no session, reject connection
+        else {
             console.error(`Socket ${socket.id} connection rejected - No active session.`);
             next(new Error("Unauthorized"));
         }
     });
-    // Setup event handlers (matchmaking, game actions, etc)
-    (0, matchmaking_1.setupMatchmaking)(io); // Make sure this happens after session/auth middleware
+    // 🔥 Add this to handle chat events
+    io.on('connection', (socket) => {
+        (0, chat_1.default)(io, socket);
+    });
+    (0, matchmaking_1.setupMatchmaking)(io);
     console.log('Loaded Matchmaking');
     return io;
 }
--- src/sockets/chat.ts
+++ src/sockets/chat.ts
@@ -1,19 +1,33 @@
 import { Server, Socket } from 'socket.io';
 
+// Utility to get a readable timestamp
+function getTimestamp(): string {
+    return new Date().toLocaleString('en-US', { hour12: true });
+}
+
 export default function registerChatHandlers(io: Server, socket: Socket) {
     socket.on('joinRoom', ({ room, username }) => {
         socket.join(room);
         socket.to(room).emit('chatMessage', `${username} joined the game.`);
+
+        // Terminal log
+        console.log(`[${getTimestamp()}] ${username} joined room: ${room}`);
     });
 
     socket.on('chatMessage', ({ room, username, message }) => {
         io.to(room).emit('chatMessage', `${username}: ${message}`);
+
+        // Terminal log
+        console.log(`[${getTimestamp()}] ${username} says in ${room}: ${message}`);
     });
 
     socket.on('disconnecting', () => {
         const rooms = [...socket.rooms].filter(r => r !== socket.id);
         rooms.forEach(room => {
             socket.to(room).emit('chatMessage', `A player has left the game.`);
+            // Terminal log
+            console.log(`[${getTimestamp()}] A player disconnected from room: ${room}`);
         });
     });
 }
+
