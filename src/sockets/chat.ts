c009e6f37aff7ab87e9478e26ade272f803f8c98
Alex3096 <bescobar5@sfsu.edu>
2025-05-21 17:38:41 -0700

    lobby chat

--- src/sockets/chat.ts
+++ src/sockets/chat.ts
@@ -5,23 +5,39 @@ function getTimestamp(): string {
 }
 
 export default function registerChatHandlers(io: Server, socket: Socket) {
+    // Join game room
     socket.on('joinRoom', ({ room, username }) => {
         socket.join(room);
-        io.to(room).emit('chatMessage', { message: `${username} joined the game.` }); // Removed `username: ''`
+        io.to(room).emit('chatMessage', { message: `${username} joined the game.` });
         console.log(`[${getTimestamp()}] ${username} joined room: ${room}`);
     });
+
+    // Chat message inside game room
     socket.on('chatMessage', ({ room, username, message }) => {
         const formattedMessage = `${username}: ${message}`;
         io.to(room).emit('chatMessage', { username, message });
         console.log(`[${getTimestamp()}] ${formattedMessage} in ${room}`);
     });
 
+    // Disconnect cleanup
     socket.on('disconnecting', () => {
         const rooms = [...socket.rooms].filter(r => r !== socket.id);
         rooms.forEach(room => {
-            socket.to(room).emit('chatMessage', { message: `A player has left the game.` }); // Removed `username: ''`
+            socket.to(room).emit('chatMessage', { message: `A player has left the game.` });
             console.log(`[${getTimestamp()}] A player disconnected from room: ${room}`);
         });
+    });
+
+    // Join lobby
+    socket.on('joinLobby', ({ username }) => {
+        socket.join('lobby');
+        console.log(`[${getTimestamp()}] ${username} joined the lobby`);
+    });
+
+    // Lobby chat message
+    socket.on('lobbyMessage', ({ username, message }) => {
+        console.log(`[${getTimestamp()}] Broadcasting message in lobby: ${username}: ${message}`);
+        io.to('lobby').emit('lobbyMessage', { username, message });
     });
 }
 
